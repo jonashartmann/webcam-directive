7579d565f5fb2ae04d14b9d16be2921edb44e0d5
Jonas Hartmann <jhartmann@aeriagames.com>
2013-12-11 15:46:55 +0100

    [#4] Improved specs to cover almost all code branches, except the canplay event in the video element

--- test/spec/directives/webcamSpec.js
+++ test/spec/directives/webcamSpec.js
@@ -2,16 +2,47 @@
 describe('Directive: webcam', function () {
   'use strict';
 
-  var element;
+  var element,
+      mediaSpy,
+      onStreamSpy,
+      onErrorSpy,
+      onSuccessSpy;
 
   beforeEach(module('webcam'));
 
+  beforeEach(function createSpy () {
+    navigator.getMedia = mediaSpy = jasmine.createSpy('getMediaSpy');
+    onStreamSpy = jasmine.createSpy('onStreamSpy');
+    onErrorSpy = jasmine.createSpy('onErrorSpy');
+    onSuccessSpy = jasmine.createSpy('onSuccessSpy');
+  });
+
   beforeEach(inject(function ($rootScope, $compile) {
-    element = angular.element('<webcam placeholder="\'http://www.example.com/\'"></webcam>');
+    $rootScope.onStream = onStreamSpy;
+    $rootScope.onError = onErrorSpy;
+    $rootScope.onSuccess = onSuccessSpy;
+    element = angular.element(
+      '<webcam '+
+      'on-stream="onStream(stream,video)" '+
+      'on-error="onError(err)" '+
+      'on-streaming="onSuccess(video)" '+
+      'placeholder="\'http://www.example.com/\'">'+
+      '</webcam>');
     element = $compile(element)($rootScope);
     expect(element).not.toBe(null);
   }));
 
+  it('should get only video media from the navigator',
+    function() {
+      expect(mediaSpy).toHaveBeenCalled();
+      var args = mediaSpy.mostRecentCall.args;
+      expect(args[0]).not.toBeNull();
+      expect(args[0].video).toBeTruthy();
+      expect(args[0].audio).toBeFalsy(); // Needs to be changed if audio support is added
+      expect(typeof args[1]).toBe('function');
+      expect(typeof args[2]).toBe('function');
+    }
+  );
 
   it('should create a video element', function () {
     var video = element.find('video');
@@ -28,4 +59,76 @@ describe('Directive: webcam', function () {
       expect(image[0].getAttribute('class')).toBe('webcam-loader');
     }
   );
+
+  describe('on success', function () {
+    var video;
+
+    beforeEach(function () {
+      video = element.find('video')[0];
+      spyOn(video, 'play');
+      var args = mediaSpy.mostRecentCall.args;
+      args[1](null); // call success function
+    });
+
+    it('should play the video element', function() {
+      expect(video.play).toHaveBeenCalled();
+    });
+
+    it('should call the stream callback', function() {
+      expect(onStreamSpy).toHaveBeenCalled();
+    });
+
+  });
+
+  describe('on failure', function () {
+    var video;
+
+    beforeEach(function () {
+      video = element.find('video')[0];
+      spyOn(video, 'play').andCallThrough();
+      var args = mediaSpy.mostRecentCall.args;
+      args[2]('Fake Error'); // call failure function
+    });
+
+    it('should remove the placeholder', function () {
+      var image = element.find('img');
+      expect(image.length).toBe(0);
+    });
+
+    it('should call the error callback', function () {
+      expect(onErrorSpy).toHaveBeenCalled();
+      expect(onErrorSpy.mostRecentCall.args[0]).toBe('Fake Error');
+    });
+  });
+
+  describe('without user media support', function() {
+
+    beforeEach(function () {
+      navigator.getMedia = false;
+    });
+
+    beforeEach(inject(function ($rootScope, $compile) {
+      $rootScope.onStream = onStreamSpy;
+      $rootScope.onError = onErrorSpy;
+      $rootScope.onSuccess = onSuccessSpy;
+      element = angular.element(
+        '<webcam '+
+        'on-stream="onStream(stream,video)" '+
+        'on-error="onError(err)" '+
+        'on-streaming="onSuccess(video)" '+
+        'placeholder="\'http://www.example.com/\'">'+
+        '</webcam>');
+      element = $compile(element)($rootScope);
+      expect(element).not.toBe(null);
+    }));
+
+    it('should call the failure callback',
+      function() {
+        expect(onErrorSpy).toHaveBeenCalled();
+        expect(onErrorSpy.mostRecentCall.args[0]).not.toBeNull();
+        expect(onErrorSpy.mostRecentCall.args[0].code).toBe(-1);
+      }
+    );
+  });
+
 });
