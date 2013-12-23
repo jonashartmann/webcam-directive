bcd0feef3f47bc1813ab009debee493ee24cc5e8
Jonas Hartmann <jonasharty@gmail.com>
2013-12-23 12:41:16 +0100

    Change karma ci task to use Firefox

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,7 @@ module.exports = function (grunt) {
       ci: {
         configFile: 'karma.conf.js',
         singleRun: true,
-        browsers: ['PhantomJS']
+        browsers: ['Firefox']
       },
       watch: {
         configFile: 'karma.conf.js',
