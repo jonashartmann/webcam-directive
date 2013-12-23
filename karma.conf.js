4c57d6c0672d0b6f35181fe1b819b6b39c4324f1
Jonas Hartmann <jonasharty@gmail.com>
2013-12-23 12:41:42 +0100

    Increase karma capture time

--- karma.conf.js
+++ karma.conf.js
@@ -49,7 +49,7 @@ autoWatch = false;
 browsers = ['Chrome'];
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 10000;
+captureTimeout = 60000;
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
