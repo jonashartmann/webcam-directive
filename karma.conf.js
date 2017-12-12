4ae429740e48ddc895f59bb50ed082b4d1aa19e1
Jonas Hartmann <jonasharty@gmail.com>
2017-12-12 22:21:23 +0100

    Update Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,55 +1,72 @@
 // Karma configuration
+// Generated on Tue Dec 12 2017 22:19:09 GMT+0100 (W. Europe Standard Time)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'test/components/angular/angular.js',
-  'test/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/spec/**/*.js'
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// web server port
-port = 8080;
 
-// cli runner port
-runnerPort = 9100;
+    // list of files / patterns to load in the browser
+    files: [
+      'app/scripts/*.js',
+      'app/scripts/**/*.js',
+      'test/spec/**/*.js',
+      'test/components/angular-mocks/angular-mocks.js',
+      'test/components/angular/angular.js'
+    ],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 10000;
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
